08e7ec400507e64cfab370cb695c0107fecb4b46
Soeperslakkie <Soeperslakkie@gmail.com>
2024-10-10 22:38:02 +0200

    Removed duplicate accounts

--- Login.pas
+++ Login.pas
@@ -29,7 +29,7 @@ type
     LineParts: TArray<string>;
   public
     { Public declarations }
-    bFound, bCopyFound: Boolean;
+    bFound: Boolean;
   end;
 
 implementation
@@ -97,45 +97,19 @@ begin
   sUser := InputBox('Account Creation','Username?:', '');
   sPass := InputBox('Account Creation','Password?:', '');
 
+
+  // Open em'
   AssignFile(FileVar, '.\Accounts.txt');
-  Reset(FileVar);  // Open the file again cuz it just has to for some reason
-    while not Eof(FileVar) do
-    begin
-      Readln(FileVar, sLine);  // Read em'
-      // Split em'
-      LineParts := sLine.Split([' ']);
-
-      if Length(LineParts) = 2 then
-      begin
-        sFileUsername := LineParts[0];  // Looking for username
-
-        // Compare em'
-        if sUser = sFileUsername then
-        begin
-          bCopyFound := True;
-          Break;  // Leave the second something matches
-        end;
-      end;
-    end;
-
-
-  if bCopyFound then
-  begin
-    ShowMessage('Username already taken, please try again.');
-  end else
-  begin
-    // Open em'
-    AssignFile(FileVar, '.\Accounts.txt');
-    if FileExists('.\Accounts.txt') then
+  if FileExists('.\Accounts.txt') then
     Append(FileVar)  // Append em'
-    else
+  else
     Rewrite(FileVar); // Create em' (incase you delete everything smartass)
 
-    // Write em'
-    Writeln(FileVar, sUser + ' ' + sPass);
-    ShowMessage('Account details saved successfully!');
-  end;
+  // Write em'
+  Writeln(FileVar, sUser + ' ' + sPass);
+  CloseFile(FileVar);
 
+  ShowMessage('Account details saved successfully!');
 end;
 
 end.
